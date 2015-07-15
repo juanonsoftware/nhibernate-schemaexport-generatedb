23ad37a2a47e6ba4627c950d494bfc665c7ef588
juanonsoftware <juanonsoftware@github.com>
2015-07-15 18:01:24 +0700

    Add a test model

---
+++ ListOnNonGenericCollection/ListOnNonGenericCollection/Maps/ProductMap.cs
@@ -0,0 +1,22 @@
+﻿using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace ListOnNonGenericCollection.Maps
+{
+    public class ProductMap : ClassMapping<Product>
+    {
+        public ProductMap()
+        {
+            Id(e => e.Id);
+            List(e => e.Tags, cm =>
+            {
+                cm.Table("Product_Tag");
+                cm.Key(km=>km.Column("ProductId"));
+                cm.Access(Accessor.ReadOnly);
+            }, m =>
+            {
+                m.OneToMany();
+            });
+        }
+    }
+}
---
+++ ListOnNonGenericCollection/ListOnNonGenericCollection/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListOnNonGenericCollection
+{
+    public class Product
+    {
+        public Product()
+        {
+            Tags = new TagCollection();
+        }
+
+        public Guid Id { get; set; }
+        public IEnumerable<Tag> Tags { get; private set; }
+        // SchemaExport doesn't work if using ITagCollection
+        //public ITagCollection Tags { get; private set; }
+    }
+}
